4d18b16e8c3f129eb4baa775ac5641a337394cca
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-03-30 11:45:11 +0200

    v0.0.2 Classes Aangemaakt

---
+++ OdoriRails/Beheersysteem/Enum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beheersysteem
+{
+    public enum EnumTramStatus
+    {
+        Normal,
+        Cleaning,
+        Maintenance,
+        NotInUse 
+    }
+    public enum EnumSectorStatus
+    {
+        Open,
+        Closed,
+        Occupied
+    }
+}
