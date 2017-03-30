ab5d08f40bf4f0a743b9e86858da47ea4aa4261c
HIGHPOWEREDTOAS\Pedro <pedrodossantosmarques@gmail.com>
2017-03-30 14:38:09 +0200

    Added base classes

---
+++ OdoriRails/SchoonmaakReparatieSysteem/Service.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoonmaakReparatieSysteem
+{
+    enum ServiceType
+    {
+        SMALL,
+        LARGE,
+        CUSTOM
+    }
+    abstract class Service
+    {
+        private DateTime startDate;
+        private DateTime endDate;
+
+        public Service(int day, int month, int year)
+        {
+            startDate = new DateTime(day, month, year);
+        }
+
+        private void AddCleaningLog(List<string> userlist, string comment, ServiceType cleaningtype)
+        {
+            // adds database entry of cleaning service log
+        }
+        private void AddReparationLog(List<string> userlist, string deffect, string solution, ServiceType reparationtype)
+        {
+            // adds database entry of reparation service log
+        }
+        private void EditLog(List<string> userlist, string comment, ServiceType cleaningtype)
+        {
+            // edits current entry of cleaning log
+        }
+        private void EditLog(List<User> userlist, string deffect, string solution, ServiceType reparationtype)
+        {
+            // edits current entry of cleaning log
+        }
+        private void DeleteLog(int logID)
+        {
+            // deletes log entry
+        }
+        private void AssignService(List<User> users)
+        {
+            // assigns service to users
+        }
+    }
+}
