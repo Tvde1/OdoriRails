30a5a95fe76013411dd7fe6828878786d43763fd
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-04-06 10:38:19 +0200

    XML Comments bij Tram toegevoegd + Line Driver public gemaakt
    Ook line en Driver public sets gemaakt omdat dit dynamisch in het
    systeem moet gebeuren

--- OdoriRails/OdoriRails/Tram.cs
+++ OdoriRails/OdoriRails/Tram.cs
@@ -15,12 +15,39 @@
 
     public class Tram
     {
+        /// <summary>
+        /// Ophalen tramnummer
+        /// </summary>
         public int Number { get; private set; }
+
+        /// <summary>
+        /// Ophalen Tramstatus
+        /// </summary>
         public TramStatus Status { get; private set; }
-        public int Line { get; private set; }
-        public User Driver { get; private set; }
+
+        /// <summary>
+        /// Get/Set lijn waar de tram opstaat
+        /// </summary>
+        public int Line { get; set; }
+
+        /// <summary>
+        /// Get/Set bestuurder van de tram
+        /// </summary>
+        public User Driver { get; set; }
+
+        /// <summary>
+        /// Ophalen model van de tram
+        /// </summary>
         public Model Model { get; private set; }
 
+        /// <summary>
+        /// Aanmaken nieuwe tram met bestuurder
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="status"></param>
+        /// <param name="line"></param>
+        /// <param name="driver"></param>
+        /// <param name="model"></param>
         public Tram(int number, TramStatus status, int line, User driver, Model model)
         {
             Number = number;
@@ -30,6 +57,17 @@
             Model = model;
         }
 
+        /// <summary>
+        /// Minimale constructor tram zonder driver en line
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="model"></param>
+        public Tram(int number, Model model)
+        {
+            Number = number;
+            Model = model;
+            Status = TramStatus.Idle;
+        }
 
     }
 }
