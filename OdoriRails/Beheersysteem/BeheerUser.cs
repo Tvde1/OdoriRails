ec331e40e4cbc67ce844d584f1440f62d0758187
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-04-06 12:23:48 +0200

    Constructors BeheerUser Toegevoegd

--- OdoriRails/Beheersysteem/BeheerUser.cs
+++ OdoriRails/Beheersysteem/BeheerUser.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OdoriRails;
 
 namespace Beheersysteem
 {
-    class BeheerUser
+    class BeheerUser : User
     {
-        
+        public BeheerUser(string name, string email, Role role, string username, string password, string managedByUsername) : base(name, email, role, username, password, managedByUsername)
+        {
+
+        }
+
+        public BeheerUser(string name, string email, Role role): base(name, email, role)
+        {
+
+        }
+
+
     }
 }
