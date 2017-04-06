9a3ce4bda67b0ec839d5577d631224dd00132b7c
Hugo <hugovdv@outlook.com>
2017-04-06 14:21:57 +0200

    base classes added

---
+++ OdoriRails/In-Uitrit Systeem/InUitRitTram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OdoriRails;
+
+namespace In_Uitrit_Systeem
+{
+    class InUitRitTram : Tram
+    {
+        public InUitRitTram(int number, Model model) : base(number, model)
+        {
+
+        }
+    }
+}
--- OdoriRails/In-Uitrit Systeem/InUitRitUser.cs
+++ OdoriRails/In-Uitrit Systeem/InUitRitUser.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OdoriRails;
 
 namespace In_Uitrit_Systeem
 {
-    class InUitRitUser
+    class InUitRitUser : User
     {
+        public string UserName { get; private set; }
+
+        public InUitRitUser(string username, string name, string email, Role role): base(name, email, role)
+        {
+            username = UserName;
+        }
     }
 }
