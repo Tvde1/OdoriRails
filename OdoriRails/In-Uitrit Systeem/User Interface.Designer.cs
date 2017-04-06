f5cb6d1c84cad0d4938a4c8bc7b67c31c86fb32e
Hugo <hugovdv@outlook.com>
2017-04-06 15:24:11 +0200

    Interface verbeterd

--- OdoriRails/In-Uitrit Systeem/User Interface.Designer.cs
+++ OdoriRails/In-Uitrit Systeem/User Interface.Designer.cs
@@ -35,9 +35,13 @@
             this.lblTramNumber = new System.Windows.Forms.Label();
             this.gbStandplaats = new System.Windows.Forms.GroupBox();
             this.lblStandplaats = new System.Windows.Forms.Label();
+            this.rtbDetails = new System.Windows.Forms.RichTextBox();
+            this.gbDetails = new System.Windows.Forms.GroupBox();
+            this.btnService = new System.Windows.Forms.Button();
             this.gbService.SuspendLayout();
             this.gbTramNumber.SuspendLayout();
             this.gbStandplaats.SuspendLayout();
+            this.gbDetails.SuspendLayout();
             this.SuspendLayout();
             // 
             // cbCleaning
@@ -47,6 +51,7 @@
             this.cbCleaning.Name = "cbCleaning";
             this.cbCleaning.Size = new System.Drawing.Size(124, 24);
             this.cbCleaning.TabIndex = 0;
+            this.cbCleaning.TabStop = false;
             this.cbCleaning.Text = "Schoonmaak";
             this.cbCleaning.UseVisualStyleBackColor = true;
             // 
@@ -57,6 +62,7 @@
             this.cbMaintenance.Name = "cbMaintenance";
             this.cbMaintenance.Size = new System.Drawing.Size(100, 24);
             this.cbMaintenance.TabIndex = 1;
+            this.cbMaintenance.TabStop = false;
             this.cbMaintenance.Text = "Reparatie";
             this.cbMaintenance.UseVisualStyleBackColor = true;
             // 
@@ -74,9 +80,9 @@
             // gbTramNumber
             // 
             this.gbTramNumber.Controls.Add(this.lblTramNumber);
-            this.gbTramNumber.Location = new System.Drawing.Point(177, 12);
+            this.gbTramNumber.Location = new System.Drawing.Point(12, 174);
             this.gbTramNumber.Name = "gbTramNumber";
-            this.gbTramNumber.Size = new System.Drawing.Size(200, 139);
+            this.gbTramNumber.Size = new System.Drawing.Size(256, 181);
             this.gbTramNumber.TabIndex = 2;
             this.gbTramNumber.TabStop = false;
             this.gbTramNumber.Text = "Tramnummer:";
@@ -86,9 +92,10 @@
             this.lblTramNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTramNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
             this.lblTramNumber.Location = new System.Drawing.Point(6, 22);
             this.lblTramNumber.Name = "lblTramNumber";
-            this.lblTramNumber.Size = new System.Drawing.Size(188, 114);
+            this.lblTramNumber.Size = new System.Drawing.Size(244, 156);
             this.lblTramNumber.TabIndex = 0;
             this.lblTramNumber.Text = "Niet bekend";
             this.lblTramNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -96,7 +103,7 @@
             // gbStandplaats
             // 
             this.gbStandplaats.Controls.Add(this.lblStandplaats);
-            this.gbStandplaats.Location = new System.Drawing.Point(12, 175);
+            this.gbStandplaats.Location = new System.Drawing.Point(274, 174);
             this.gbStandplaats.Name = "gbStandplaats";
             this.gbStandplaats.Size = new System.Drawing.Size(365, 181);
             this.gbStandplaats.TabIndex = 4;
@@ -108,6 +115,7 @@
             this.lblStandplaats.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStandplaats.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
             this.lblStandplaats.Location = new System.Drawing.Point(6, 22);
             this.lblStandplaats.Name = "lblStandplaats";
             this.lblStandplaats.Size = new System.Drawing.Size(353, 156);
@@ -115,12 +123,45 @@
             this.lblStandplaats.Text = "Niet bekend";
             this.lblStandplaats.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // rtbDetails
+            // 
+            this.rtbDetails.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtbDetails.Location = new System.Drawing.Point(6, 25);
+            this.rtbDetails.Name = "rtbDetails";
+            this.rtbDetails.Size = new System.Drawing.Size(343, 96);
+            this.rtbDetails.TabIndex = 0;
+            this.rtbDetails.Text = "";
+            // 
+            // gbDetails
+            // 
+            this.gbDetails.Controls.Add(this.rtbDetails);
+            this.gbDetails.Location = new System.Drawing.Point(187, 12);
+            this.gbDetails.Name = "gbDetails";
+            this.gbDetails.Size = new System.Drawing.Size(355, 139);
+            this.gbDetails.TabIndex = 6;
+            this.gbDetails.TabStop = false;
+            this.gbDetails.Text = "Details:";
+            // 
+            // btnService
+            // 
+            this.btnService.BackColor = System.Drawing.SystemColors.Window;
+            this.btnService.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnService.Location = new System.Drawing.Point(548, 21);
+            this.btnService.Name = "btnService";
+            this.btnService.Size = new System.Drawing.Size(91, 130);
+            this.btnService.TabIndex = 1;
+            this.btnService.Text = "Meld tram aan voor service";
+            this.btnService.UseVisualStyleBackColor = false;
+            this.btnService.Click += new System.EventHandler(this.btnService_Click);
+            // 
             // FormUserInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(389, 367);
+            this.ClientSize = new System.Drawing.Size(651, 367);
+            this.Controls.Add(this.btnService);
+            this.Controls.Add(this.gbDetails);
             this.Controls.Add(this.gbStandplaats);
             this.Controls.Add(this.gbTramNumber);
             this.Controls.Add(this.gbService);
@@ -134,6 +175,7 @@
             this.gbService.PerformLayout();
             this.gbTramNumber.ResumeLayout(false);
             this.gbStandplaats.ResumeLayout(false);
+            this.gbDetails.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -147,6 +189,9 @@
         private System.Windows.Forms.Label lblTramNumber;
         private System.Windows.Forms.GroupBox gbStandplaats;
         private System.Windows.Forms.Label lblStandplaats;
+        private System.Windows.Forms.RichTextBox rtbDetails;
+        private System.Windows.Forms.GroupBox gbDetails;
+        private System.Windows.Forms.Button btnService;
     }
 }
 
--- OdoriRails/In-Uitrit Systeem/User Interface.cs
+++ OdoriRails/In-Uitrit Systeem/User Interface.cs
@@ -16,5 +16,10 @@ namespace Beheersysteem
         {
             InitializeComponent();
         }
+
+        private void btnService_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
