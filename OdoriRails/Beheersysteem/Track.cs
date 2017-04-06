3f6d8bc553da434bb41f92b7fd6cee1615ccdf6b
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-04-06 12:04:33 +0200

    Handmatige Opschoning Files ivm Merge Calamiteiten

---
+++ OdoriRails/Beheersysteem/Track.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OdoriRails;
+
+namespace Beheersysteem
+{
+    class Track : OdoriRails.Track
+    {
+        public List<Sector> BeheerSectors = new List<Sector>();
+
+        public Track(int tracknumber) : base(tracknumber)
+        { }
+
+        /// <summary>
+        /// Zet elke sectoren's status op 'Locked'
+        /// </summary>
+        public void LockTrack()
+        {
+            foreach (var sector in BeheerSectors)
+            {
+                sector.Lock();
+            }
+        }
+
+        /// <summary>
+        /// Zet alle sectoren's status op 'Open'.
+        /// </summary>
+        public void UnlockTrack()
+        {
+            foreach (var sector in BeheerSectors)
+            {
+                sector.Unlock();
+            }
+        }
+    }
+}
--- OdoriRails/Beheersysteem/User.cs
+++ OdoriRails/Beheersysteem/User.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SchoonmaakReparatieSysteem
+namespace Beheersysteem
 {
-    class Schoonmaak
+    class User
     {
+        
     }
 }
