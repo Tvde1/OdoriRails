06366248d6918c1bf01b9af7d2936dd0a9202bb8
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-04-07 10:05:00 +0200

    Andere database aanroepen

--- OdoriRails/User Beheersysteem/User Interface.cs
+++ OdoriRails/User Beheersysteem/User Interface.cs
@@ -13,7 +13,7 @@ namespace User_Beheersysteem
 {
     public partial class UserInterface : Form
     {
-        IDatabaseConnector databaseConnector = new MSSQLDatabaseContext();
+        //IDatabaseConnector databaseConnector = new MySqlContect();
         private List<User> Users = new List<User>();
 
         public UserInterface()
