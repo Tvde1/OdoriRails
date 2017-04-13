986d664c2e9f7ec8578572e80ab8e712ddcd943f
Hugo <hugovdv@outlook.com>
2017-04-13 13:14:25 +0200

    Merged Login

--- OdoriRails/InlogSysteem/Program.cs
+++ OdoriRails/InlogSysteem/Program.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using In_Uitrit_Systeem;
-using OdoriRails;
 using LoginSystem;
 
 namespace InlogSysteem
@@ -15,11 +13,11 @@ namespace InlogSysteem
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main(User driver)
+        static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormUserInterface(driver));
+            Application.Run(new LoginScreen());
         }
     }
 }
