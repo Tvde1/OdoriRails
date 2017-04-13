e058f73c604b0fbb6dccdc95a4c7b00e1f255de9
Hugo <hugovdv@outlook.com>
2017-04-13 13:23:31 +0200

    Small loginUI tweaks

--- OdoriRails/InlogSysteem/InlogScreen.Designer.cs
+++ OdoriRails/InlogSysteem/InlogScreen.Designer.cs
@@ -42,42 +42,48 @@
             // 
             // tbUsername
             // 
-            this.tbUsername.Location = new System.Drawing.Point(234, 117);
+            this.tbUsername.Location = new System.Drawing.Point(351, 180);
+            this.tbUsername.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbUsername.Name = "tbUsername";
-            this.tbUsername.Size = new System.Drawing.Size(164, 20);
+            this.tbUsername.Size = new System.Drawing.Size(244, 26);
             this.tbUsername.TabIndex = 0;
             // 
             // tbPassword
             // 
-            this.tbPassword.Location = new System.Drawing.Point(234, 181);
+            this.tbPassword.Location = new System.Drawing.Point(351, 278);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbPassword.Name = "tbPassword";
-            this.tbPassword.Size = new System.Drawing.Size(164, 20);
+            this.tbPassword.PasswordChar = '•';
+            this.tbPassword.Size = new System.Drawing.Size(244, 26);
             this.tbPassword.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(231, 101);
+            this.label1.Location = new System.Drawing.Point(346, 155);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.Size = new System.Drawing.Size(91, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Username:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(231, 165);
+            this.label2.Location = new System.Drawing.Point(346, 254);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.Size = new System.Drawing.Size(88, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Password:";
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(323, 207);
+            this.btnLogin.Location = new System.Drawing.Point(484, 318);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(75, 23);
-            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Size = new System.Drawing.Size(112, 35);
+            this.btnLogin.TabIndex = 3;
             this.btnLogin.Text = "Log In";
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
@@ -85,9 +91,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::LoginSystem.Properties.Resources.temp;
-            this.pictureBox2.Location = new System.Drawing.Point(423, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(634, 18);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(197, 292);
+            this.pictureBox2.Size = new System.Drawing.Size(296, 449);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
@@ -95,9 +102,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::LoginSystem.Properties.Resources.temp;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(18, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(197, 292);
+            this.pictureBox1.Size = new System.Drawing.Size(296, 449);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -106,17 +114,19 @@
             // 
             this.lbError.AutoSize = true;
             this.lbError.ForeColor = System.Drawing.Color.Red;
-            this.lbError.Location = new System.Drawing.Point(12, 307);
+            this.lbError.Location = new System.Drawing.Point(18, 472);
+            this.lbError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbError.Name = "lbError";
-            this.lbError.Size = new System.Drawing.Size(49, 13);
+            this.lbError.Size = new System.Drawing.Size(79, 20);
             this.lbError.TabIndex = 6;
             this.lbError.Text = "Error text";
             // 
             // LoginScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AcceptButton = this.btnLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(646, 328);
+            this.ClientSize = new System.Drawing.Size(969, 505);
             this.Controls.Add(this.lbError);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
@@ -125,8 +135,11 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tbPassword);
             this.Controls.Add(this.tbUsername);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "LoginScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "OdoriRails Login";
             this.Load += new System.EventHandler(this.InlogScreen_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
