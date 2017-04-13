e735934807de228fda7108897893c4a903fe3a80
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-04-13 10:53:40 +0200

    Master Merge Handmatig Uitgevoerd

---
+++ OdoriRails/OdoriRails/BaseClasses/Cleaning.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OdoriRails
+{
+    public enum CleaningSize
+    {
+        Big,
+        Small
+    }
+
+    class Cleaning : Service
+    {
+        public CleaningSize Size { get; protected set; }
+        public string Comments { get; protected set; }
+
+       public Cleaning(int id, DateTime startDate, DateTime endDate, CleaningSize size, string comments, List<User> users, int tramId) : base(id, users, startDate, endDate, tramId)
+        {
+            Size = size;
+            Comments = comments;
+        }
+
+        public Cleaning(DateTime startDate, DateTime endDate, CleaningSize size, string comments, List<User> users, int tramId) : base(users, startDate, endDate, tramId)
+        {
+            Size = size;
+            Comments = comments;
+        }
+    }
+}
