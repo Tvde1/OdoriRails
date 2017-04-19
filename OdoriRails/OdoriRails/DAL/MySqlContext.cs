c217311c9de1dc77bcfe3967d6f1e222e037f783
Tvde1 <tim3345@live.nl>
2017-04-19 14:38:23 +0200

    ok

--- OdoriRails/OdoriRails/DAL/MySqlContext.cs
+++ OdoriRails/OdoriRails/DAL/MySqlContext.cs
@@ -226,7 +226,7 @@ FROM Service INNER JOIN
 (SELECT ServiceUser.ServiceCk
 FROM ServiceUser INNER JOIN
 User ON ServiceUser.UserCk = User.UserPk
-WHERE (User.UserPk = @id)) AS derivedtbl_1 ON Service.ServicePk = derivedtbl_1.ServiceCk) AS derivedtbl_2 ON Repair.ServiceFk = derivedtbl_2.ServicePk";
+WHERE (User.UserPk = @id)) AS derivedtbl_1 ON Service.ServicePk = derivedtbl_1.ServiceCk) AS derivedtbl_2 ON Clean.ServiceFk = derivedtbl_2.ServicePk";
 
             var repairQuery = new MySqlCommand(repairs);
             repairQuery.Parameters.AddWithValue("@id", user.Id);
