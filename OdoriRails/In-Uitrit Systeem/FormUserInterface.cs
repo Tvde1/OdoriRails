3f2fa09a29285d678ac2ff651eef8f014a655f2d
Hugo <hugovdv@outlook.com>
2017-04-19 09:31:52 +0200

    Manual merge with master

--- OdoriRails/In-Uitrit Systeem/FormUserInterface.cs
+++ OdoriRails/In-Uitrit Systeem/FormUserInterface.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using OdoriRails;
+using OdoriRails.BaseClasses;
 
 namespace In_Uitrit_Systeem
 {
---
+++ OdoriRails/OdoriRails/BaseClasses/Tram.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace OdoriRails.BaseClasses
+{
+    public enum TramStatus
+    {
+        Idle,
+        Cleaning,
+        Maintenance,
+        CleaningMaintenance,
+        Defect
+    }
+
+    public enum TramLocation
+    {
+        Home,
+        Away,
+        Entering
+    }
+
+    public enum Model
+    {
+        Classic
+    }
+
+    public class Tram
+    {
+        /// <summary>
+        /// Ophalen tramnummer
+        /// </summary>
+        public int Number { get; protected set; }
+
+        /// <summary>
+        /// Ophalen Tramstatus
+        /// </summary>
+        public TramStatus Status { get; protected set; }
+
+        /// <summary>
+        /// Get/Set lijn waar de tram opstaat
+        /// </summary>
+        public int Line { get; protected set; }
+
+        /// <summary>
+        /// Get/Set bestuurder van de tram
+        /// </summary>
+        public User Driver { get; protected set; }
+
+        /// <summary>
+        /// Ophalen model van de tram
+        /// </summary>
+        public Model Model { get; protected set; }
+
+        public DateTime DepartureTime { get; set; }
+
+        /// <summary>
+        /// Aanmaken nieuwe tram met bestuurder
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="status"></param>
+        /// <param name="line"></param>
+        /// <param name="driver"></param>
+        /// <param name="model"></param>
+        public Tram(int number, TramStatus status, int line, User driver, Model model)
+        {
+            Number = number;
+            Status = status;
+            Line = line;
+            Driver = driver;
+            Model = model;
+        }
+
+        /// <summary>
+        /// Minimale constructor tram zonder driver en line
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="model"></param>
+        public Tram(int number, Model model)
+        {
+            Number = number;
+            Model = model;
+            Status = TramStatus.Idle;
+        }
+
+    }
+}
