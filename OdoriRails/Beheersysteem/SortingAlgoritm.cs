e873d4e24b65e40091063ff7163dbac9325c6088
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-04-19 08:56:28 +0200

    Revert "Revert "Update van algorit van algoritme""
    This reverts commit 8def3f5fee40b193eb2aaa03a8149293c0c466bd.

--- OdoriRails/Beheersysteem/SortingAlgoritm.cs
+++ OdoriRails/Beheersysteem/SortingAlgoritm.cs
@@ -36,50 +36,54 @@ namespace Beheersysteem
             //Else uses normal algorithem
             else
             {
+                List<Track> potentialTracks = new List<Track>();
                 //Put tram on track thats connected to the line the tram is on
                 foreach (Track track in allTracks)
                 {
                     if (track.Line == tram.Line && track.Type == TrackType.Normal)
                     {
-                        foreach (Sector sector in track.Sectors)
-                        {
-                            if (sector.Status == SectorStatus.Open)
-                            {
-                                return sector;
-                            }
-                        }
+                        potentialTracks.Add(track);
                     }
                 }
+
+                Sector tempSector = GetPotentialSector(potentialTracks);
+                if (tempSector != null)
+                {
+                    return tempSector;
+                }
+
+                potentialTracks.Clear();//Wipe list to start a new
 
                 //If not successful put tram on any other normal track (that doesn't have another line connected to it)
                 foreach (Track track in allTracks)
                 {
                     if (track.Type == TrackType.Normal)
                     {
-                        foreach (Sector sector in track.Sectors)
-                        {
-                            if (sector.Status == SectorStatus.Open)
-                            {
-                                return sector;
-                            }
-                        }
+                        potentialTracks.Add(track);
                     }
+                }
+
+                tempSector = GetPotentialSector(potentialTracks);
+                if (tempSector != null)
+                {
+                    return tempSector;
                 }
 
                 //If not successful put on an exit line
                 foreach (Track track in allTracks)
                 {
-                    if (track.Line == tram.Line && track.Type == TrackType.Exit)
+                    if (track.Type == TrackType.Exit)
                     {
-                        foreach (Sector sector in track.Sectors)
-                        {
-                            if (sector.Status == SectorStatus.Open)
-                            {
-                                return sector;
-                            }
-                        }
+                        potentialTracks.Add(track);
                     }
                 }
+
+                tempSector = GetPotentialSector(potentialTracks);
+                if (tempSector != null)
+                {
+                    return tempSector;
+                }
+
             }
             //If not successful let user place tram
             return GetSector(tram);
@@ -117,5 +121,24 @@ namespace Beheersysteem
             }
             return null;
         }
+
+        private Sector GetPotentialSector(List<Track> potentialTracks)
+        {
+            foreach (Track track in potentialTracks)
+            {
+                for (int i = 0; i < track.Sectors.Count - 1; i++)
+                {
+                    if (track.Sectors[i].Status == SectorStatus.Occupied && track.Sectors[i].OccupyingTram.DepartureTime < tram.DepartureTime)
+                    {
+                        if (track.Sectors[i + 1].Status == SectorStatus.Open)
+                        {
+                            return track.Sectors[i + 1];
+                        }
+                    }
+                }
+            }
+            return null;
+        }
     }
+
 }
