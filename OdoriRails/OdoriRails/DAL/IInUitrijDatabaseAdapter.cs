6602c28d904a581643eb5477554f389e15fdbbb1
Tvde1 <tim3345@live.nl>
2017-04-19 22:12:06 +0200

    yay meer werk

--- OdoriRails/OdoriRails/DAL/IInUitrijDatabaseAdapter.cs
+++ OdoriRails/OdoriRails/DAL/IInUitrijDatabaseAdapter.cs
@@ -1,13 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OdoriRails.BaseClasses;
 
 namespace OdoriRails.DAL
 {
     public interface IInUitrijDatabaseAdapter
     {
+        /// <summary>
+        /// Voegt een Schoonmaak toe en geeft de schoonmaak met ID terug.
+        /// </summary>
+        /// <param name="cleaning"></param>
+        /// <returns></returns>
+        Cleaning AddCleaning(Cleaning cleaning);
 
+        /// <summary>
+        /// Voegt een Repair toe en geeft de repair met ID terug.
+        /// </summary>
+        /// <param name="repair"></param>
+        /// <returns></returns>
+        Repair AddRepair(Repair repair);
+
+        /// <summary>
+        /// Get de user ID via de username.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        int GetUserId(string username);
     }
 }
