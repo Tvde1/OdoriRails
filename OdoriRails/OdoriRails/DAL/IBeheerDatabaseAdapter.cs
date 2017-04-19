9726c02bff2d55defc1bc0e6937bbca71cffcaae
Tvde1 <tim3345@live.nl>
2017-04-19 23:25:10 +0200

    Opnieuw proberen

---
+++ OdoriRails/OdoriRails/DAL/IBeheerDatabaseAdapter.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using OdoriRails.BaseClasses;
+
+namespace OdoriRails.DAL
+{
+    public interface IBeheerDatabaseAdapter
+    {
+
+        /// <summary>
+        /// Voegt een User toe aan de database.
+        /// </summary>
+        /// <param name="user"></param>
+        User AddUser(User user);
+
+        /// <summary>
+        /// Haalt alle users op.
+        /// </summary>
+        /// <returns></returns>
+        List<User> GetAllUsers();
+
+        /// <summary>
+        /// Verwijdert een User uit de database.
+        /// </summary>
+        /// <param name="user"></param>
+        void RemoveUser(User user);
+
+        /// <summary>
+        /// Haal een User op aan de hand van de userid.
+        /// </summary>
+        /// <param name="id"></param>
+        User GetUser(int id);
+
+        /// <summary>
+        /// Get de user ID via de username.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        int GetUserId(string username);
+
+        /// <summary>
+        /// Slaat de bestaande user op in de database.
+        /// </summary>
+        /// <param name="user"></param>
+        void UpdateUser(User user);
+
+        /// <summary>
+        /// Haal een User op aan de hand van de username.
+        /// </summary>
+        /// <param name="userName"></param>
+        User GetUser(string userName);
+
+        /// <summary>
+        /// Haalt alle users op die deze rol hebben.
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        List<User> GetAllUsersWithRole(Role role);
+    }
+}
---
+++ OdoriRails/OdoriRails/DAL/ISchoonmaakReparatieDatabaseAdapter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using OdoriRails.BaseClasses;
+
+namespace OdoriRails.DAL
+{
+    public interface ISchoonmaakReparatieDatabaseAdapter
+    {
+        /// <summary>
+        /// Haal alle services op (reparatie en schoonmaak) die deze user hebben.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        List<Service> GetAllServicesFromUser(User user);
+
+        /// <summary>
+        /// Haalt een lijst op van repairs zonder users.
+        /// </summary>
+        /// <returns></returns>
+        List<Repair> GetAllRepairsWithoutUsers();
+
+        /// <summary>
+        /// Haalt een lijst op van cleanings zonder users.
+        /// </summary>
+        /// <returns></returns>
+        List<Cleaning> GetAllCleansWithoutUsers();
+        
+        /// <summary>
+        /// Voegt een Schoonmaak toe en geeft de schoonmaak met ID terug.
+        /// </summary>
+        /// <param name="cleaning"></param>
+        /// <returns></returns>
+        Cleaning AddCleaning(Cleaning cleaning);
+
+        /// <summary>
+        /// Voegt een Repair toe en geeft de repair met ID terug.
+        /// </summary>
+        /// <param name="repair"></param>
+        /// <returns></returns>
+        Repair AddRepair(Repair repair);
+
+        /// <summary>
+        /// Past de service aan in de database.
+        /// </summary>
+        /// <param name="service"></param>
+        void EditService(Service service);
+
+        /// <summary>
+        /// Verweider een service uit de database.
+        /// </summary>
+        /// <param name="service"></param>
+        void DeleteService(Service service);
+    }
+}
