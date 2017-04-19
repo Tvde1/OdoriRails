a6582ba080c127eee97cc998a838e18606780917
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-04-19 09:21:29 +0200

    Simulatie toegevoegd (nog niet getest)

--- OdoriRails/Beheersysteem/Logic.cs
+++ OdoriRails/Beheersysteem/Logic.cs
@@ -45,5 +45,37 @@ namespace Beheersysteem
                 System.Windows.Forms.MessageBox.Show("Het systeem kan geen passende plek vinden voor deze tram. Plaats deze tram manueel.");
             }
         }
+
+        public void Simulation(List<Tram> allTrams)
+        {
+            //De schema moet op volgorde van eerst binnenkomende worden gesorteerd
+            schema.Sort((x, y) => x.EntryTime.CompareTo(y.EntryTime));
+
+            //Voor iedere inrijtijd een tram eraan koppellen
+            foreach (InUitRijSchema entry in schema)
+            {
+                if (entry.TramNumber == null)
+                {
+                    foreach (Tram tram in allTrams)
+                    {
+                        if (tram.Line == entry.Line && tram.DepartureTime == null)
+                        {
+                            entry.TramNumber = tram.Number;
+                            tram.DepartureTime = entry.ExitTime;
+                            continue;
+                        }
+                    }
+
+                }
+            }
+
+            //Het schema afgaan voor de simulatie
+            foreach (InUitRijSchema entry in schema)
+            {
+                Tram tram = allTrams.Find(x => x.Number == entry.TramNumber);
+                SortTram(tram);
+            }
+
+        }
     }
 }
