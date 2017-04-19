de5f25ca93216b41381e86d8bd323ba5245c9d80
Tvde1 <tim3345@live.nl>
2017-04-19 18:22:36 +0200

    big change in Interface architecture

---
+++ OdoriRails/OdoriRails/DAL/ILoginDatabaseAdapter.cs
@@ -0,0 +1,20 @@
+﻿namespace OdoriRails.DAL
+{
+    public interface ILoginDatabaseAdapter
+    {
+        /// <summary>
+        /// Kijkt of de username bestaat in de database.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        bool ValidateUsername(string username);
+
+        /// <summary>
+        /// Een bool om de gebruikersnaam en wachtwoord te controleren.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        bool MatchUsernameAndPassword(string username, string password);
+    }
+}
