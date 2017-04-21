a4d745548e618523750de5c82ee6b938b54f5639
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-04-21 10:44:15 +0200

    Update van master

--- OdoriRails/OdoriRails/DAL/ITrackSectorContext.cs
+++ OdoriRails/OdoriRails/DAL/ITrackSectorContext.cs
@@ -5,6 +5,20 @@ namespace OdoriRails.DAL
 {
     public interface ITrackSectorContext
     {
+        /// <summary>
+        /// Haalt alle tracks, sectoren en trams op.
+        /// </summary>
+        /// <returns></returns>
         List<Track> GetTracksAndSectors();
+
+        /// <summary>
+        /// Updated de track in de database met de nieuwe informatie.
+        /// </summary>
+        void EditTrack(Track track);
+
+        /// <summary>
+        /// Updated de sector in de database met de nieuwe informatie.
+        /// </summary>
+        void EditSector(Sector sector);
     }
 }
