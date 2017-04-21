f6b426c42b9ae5b58b76d105268334abe3a84bda
Tvde1 <tim3345@live.nl>
2017-04-21 17:13:34 +0200

    weg hiermee

--- OdoriRails/OdoriRails/DAL/ILoginContext.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OdoriRails.DAL
-{
-    public interface ILoginContext
-    {
-        bool ValidateUsername(string username);
-
-        bool MatchUsernameAndPassword(string username, string password);
-    }
-}
