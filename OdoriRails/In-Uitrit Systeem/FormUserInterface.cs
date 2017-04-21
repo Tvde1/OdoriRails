997aa78953e419b1e3685b2ea9c15c1d3c6ff59a
Hugo <hugovdv@outlook.com>
2017-04-21 12:02:34 +0200

    Added uitrij systeem

--- OdoriRails/In-Uitrit Systeem/FormUserInterface.cs
+++ OdoriRails/In-Uitrit Systeem/FormUserInterface.cs
@@ -13,16 +13,16 @@ namespace In_Uitrit_Systeem
         {
             InitializeComponent();
             _logic = new Logic(driver);
-            _lineFetcher = new Timer { Interval = 10000 };
+            _lineFetcher = new Timer { Interval = 5000 };
             _lineFetcher.Tick += LineFetcher_Tick;
             lblTramNumber.Text = _logic.Tram?.Number.ToString() ?? "Geen Tram";
         }
 
         private void LineFetcher_Tick(object sender, EventArgs e)
         {
-            if (_logic.Tram.Line.ToString() != lblStandplaats.Text)
+            if (_logic.Tram.Location == TramLocation.In)
             {
-                lblStandplaats.Text = _logic.Tram.Line.ToString();
+                //lblStandplaats.Text = _logic.Tram.Line.ToString();    <-- klopt niet, moet een combinatie zijn van track + sector
                 _lineFetcher.Stop();
             }
         }
@@ -41,6 +41,7 @@ namespace In_Uitrit_Systeem
                 MessageBox.Show(error, "Fout", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
+
             string defect = rtbDetails.Text;
             if (cbCleaning.Checked && cbMaintenance.Checked)
             {
@@ -58,14 +59,36 @@ namespace In_Uitrit_Systeem
                 _logic.Tram.EditTramStatus(TramStatus.Maintenance);
                 _logic.AddRepair(defect);
             }
+
             rtbDetails.Text = "";
-            btnService.Enabled = false;
-            cbCleaning.Enabled = false;
-            cbMaintenance.Enabled = false;
             rtbDetails.Enabled = false;
+            cbCleaning.Checked = false;
+            cbMaintenance.Checked = false;
+
             _logic.Tram.EditTramLocation(TramLocation.ComingIn);
-            lblStandplaats.Text = _logic.Tram.Line.ToString();
             _lineFetcher.Start();
+        }
+
+        private void btnLeave_Click(object sender, EventArgs e)
+        {
+            if (_logic.Tram == null)
+            {
+                MessageBox.Show("U bestuurt nu geen tram.");
+                return;
+            }
+
+            if (_logic.Tram.Location == TramLocation.In || _logic.Tram.Location == TramLocation.ComingIn)
+            {
+                string error = string.Format("Tram {0} is nog niet aangemeld bij de remise. Meld deze tram eerst aan bij de remise.", _logic.Tram.Number.ToString());
+                MessageBox.Show(error, "Fout", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            lblStandplaats.Text = "Niet bekend";
+
+            _logic.Tram.EditTramLocation(TramLocation.Out);
+            _logic.Tram.ResetTramDeparture();
+            _logic.Tram.ResetTramTrackAndSector();
         }
 
         private void cbMaintenance_CheckedChanged(object sender, EventArgs e)
