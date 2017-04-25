d7b4d16fa16577ca004b332d4cef8142fa96ad4e
Tvde1 <tim3345@live.nl>
2017-04-25 14:55:12 +0200

    repository edit

--- OdoriRails/OdoriRailsTests1/RepositoryTests.cs
+++ OdoriRails/OdoriRailsTests1/RepositoryTests.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OdoriRails.BaseClasses;
-using OdoriRails.DAL.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OdoriRails.DAL.Repository.Tests
 {
