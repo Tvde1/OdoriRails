881bfecf50bbd67c36de18c1d944903c4403af70
Tvde1 <tim3345@live.nl>
2017-05-01 14:02:48 +0200

    Added custom database exception

---
+++ OdoriRails/OdoriRails/DAL/DatabaseException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OdoriRails.DAL
+{
+    public class DatabaseException : Exception
+    {
+        public DatabaseException(string message) : base(message)
+        {
+        }
+    }
+}
