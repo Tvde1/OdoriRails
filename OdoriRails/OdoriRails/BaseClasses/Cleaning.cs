17149591c58c6e786855266715eef5d3ff4177bd
Tvde1 <tim3345@live.nl>
2017-05-04 10:32:44 +0200

    vergeten deze bestanden te pushen

--- OdoriRails/OdoriRails/BaseClasses/Cleaning.cs
+++ OdoriRails/OdoriRails/BaseClasses/Cleaning.cs
@@ -14,7 +14,7 @@ namespace OdoriRails.BaseClasses
         public CleaningSize Size { get; protected set; }
         public string Comments { get; protected set; }
 
-       public Cleaning(int id, DateTime startDate, DateTime endDate, CleaningSize size, string comments, List<User> users, int tramId) : base(id, users, startDate, endDate, tramId)
+       public Cleaning(int id, DateTime startDate, DateTime? endDate, CleaningSize size, string comments, List<User> users, int tramId) : base(id, users, startDate, endDate, tramId)
         {
             Size = size;
             Comments = comments;
